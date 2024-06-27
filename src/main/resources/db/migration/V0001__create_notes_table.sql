aecf204e22ec713a074ad9e15fc49b2bd26ee031
Rafael Roman <panthro.rafael@gmail.com>
2024-06-27 13:43:53 +0200

    implement JDBCNoteRepository.archive

--- src/main/resources/db/migration/V0001__create_notes_table.sql
+++ src/main/resources/db/migration/V0001__create_notes_table.sql
@@ -1,7 +1,7 @@
 -- Create the notes table
 CREATE TABLE notes
 (
-    id       SERIAL PRIMARY KEY,
+    id       uuid PRIMARY KEY,
     title    VARCHAR(255)          NOT NULL,
     content  TEXT                  NOT NULL,
     archived BOOLEAN DEFAULT FALSE NOT NULL
